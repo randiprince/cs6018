5ee496ae67a5686debd0d8685fa269cc680fda79
randiprince <u1431008@umail.utah.edu>
2023-09-26 09:23:42 -0600

    lab4 completed

---
+++ lab4/app/src/main/java/com/example/lab4/JokeRepository.kt
@@ -0,0 +1,20 @@
+package com.example.lab4
+
+import androidx.lifecycle.asLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.Date
+class JokeRepository( val scope: CoroutineScope, val dao: JokeDAO) {
+
+    val currentJoke = dao.latestJoke().asLiveData()
+
+    val allJokes = dao.allJokes().asLiveData()
+    fun checkJoke(joke: String){
+        scope.launch {
+            delay(1000) // pretend this is a slow network call
+            dao.addJokeData(
+                JokeData(Date(), joke))
+        }
+    }
+}
